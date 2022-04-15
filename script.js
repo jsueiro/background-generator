bfbd303961f3786d65f3554eff6286267ce59815
jsueiro <juan.sueiro@outlook.com>
2022-04-15 18:09:58 +0200

    added copy function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let body = document.getElementById('gradient');
 
+
 function setGradient() {
     body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
     css.textContent = body.style.background + ';';
@@ -12,6 +13,19 @@ window.onload = function () {
     css.textContent = 'linear-gradient(to right, #80bcea, #ebedf3);';
 
 }
+
+function copyStyle() {
+
+    navigator.clipboard.writeText(css.textContent).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+        console.log(css.textContent);
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+
+}
+
+css.addEventListener('click', copyStyle);
 
 color1.addEventListener('input', setGradient);
 
